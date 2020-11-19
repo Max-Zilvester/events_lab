d5c8c157fc910bfb558a59b59b7930b3e765ed1e
Ruth Foulis <ruthlilfoulis@gmail.com>
2020-11-19 16:31:46 +0000

    mvp completed

--- lab_events_start/js/app.js
+++ lab_events_start/js/app.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript loaded');
-})
+  //console.log('JavaScript loaded');
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleForm);
+
+  
+});
+
+  const handleForm = function(event){
+  event.preventDefault();
+  const newReadingListItem = document.createElement('li');
+  newReadingListItem.textContent = `Title: ${event.target.title.value} 
+  Author: ${event.target.author.value}
+  Category: ${event.target.category.value}
+  `
+  const list = document.querySelector('ul');   
+  list.appendChild(newReadingListItem);
+  document.getElementById("new-item-form").reset();
+
+};
+
+
+
+
